32ce76305d5b8118da6abf8cf050933a3432c828
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-06-11 01:56:38 +0800

    Initialize an array and create for loop to store the binary numbers

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -7,6 +7,14 @@ public class BinaryToDecimal {
         System.out.print("Enter the number of binary number(s) you want to convert: ");
         int size = scanner.nextInt();
 
+        int[] binaryArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter the binary number: ");
+            int binary = scanner.nextInt();
+            binaryArray[i] = binary;
+        }
+
         scanner.close();
     }
 }
