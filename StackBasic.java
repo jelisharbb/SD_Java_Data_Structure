d71570e7c9c17a9ed768434defc7db8817748d40
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-06-12 00:30:29 +0800

    Add if else to push, pop, and peek elements

--- StackBasic.java
+++ StackBasic.java
@@ -12,23 +12,39 @@ public class StackBasic {
 
     // Add element to top of stack
     public void push(int value) {
-
+        if (top == maxSize - 1) {
+            System.out.println("Stack is full. Cannot push element.");
+        } else {
+            top++;
+            stackArray[top] = value;
+        }
     }
 
     // Remove element from top of stack
     public void pop() {
-
+        if (top == -1) {
+            System.out.println("Stack is empty. Cannot pop element.");
+        } else {
+            top--;
+        }
     }
 
     // Return top of stack
     public int peek() {
-
+        if (top >= 0) {
+            return stackArray[top];
+        } else {
+            System.out.println("Stack is empty.");
+            return -1;
+        }
     }
 
     public static void main(String args[]) {
     StackBasic stack = new StackBasic(5);
     stack.push(1);
     stack.push(2);
+    stack.push(3);
+    stack.push(4);
     System.out.println("Top element: " + stack.peek());
     stack.pop();
     System.out.println("Top element: " + stack.peek());
