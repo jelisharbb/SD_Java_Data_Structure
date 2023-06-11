b24171f5db0af3a9e5eac8a847d4b63b8e547f2f
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-06-11 16:23:15 +0800

    Add a for loop that will convert the given binary to decimals and print them

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -8,13 +8,23 @@ public class BinaryToDecimal {
         int size = scanner.nextInt();
 
         int[] binaryArray = new int[size];
-
         for (int i = 0; i < size; i++) {
-            System.out.print("Enter the binary number: ");
+            System.out.print("Binary number " + (i + 1) + ": ");
             int binary = scanner.nextInt();
             binaryArray[i] = binary;
         }
 
         scanner.close();
+
+        int[] decimalArray = new int[binaryArray.length];
+        for (int i = 0; i < binaryArray.length; i++) {
+            String binaryString = String.valueOf(binaryArray[i]); 
+            decimalArray[i] = Integer.parseInt(binaryString, 2);
+        }
+
+        for (int i = 0; i < decimalArray.length; i++) {
+            int decimal = decimalArray[i];
+            System.out.println("Decimal number " + (i + 1) + ": " + decimal);
+        }
     }
 }
