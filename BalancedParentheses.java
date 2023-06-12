87a3de65a1d98244e22a8666002b1a198c906131
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-06-12 17:09:24 +0800

    Implement a method to determine if the parentheses are balanced or not

--- BalancedParentheses.java
+++ BalancedParentheses.java
@@ -9,8 +9,27 @@ public class BalancedParentheses {
     String[] parentheses = new String[numberOfTest];
     for (int i = 0; i < numberOfTest; i++) {
       parentheses[i] =  scanner.nextLine();
-      
-    scanner.close();
     }
+
+    String[] results = checkBalancedParentheses(parentheses);
+    for (String result: results) {
+      System.out.println(result);
+    }
+
+    scanner.close();
+  }
+
+  public static String[] checkBalancedParentheses(String[] parentheses) {
+    String[] results = new String[parentheses.length];
+    
+    for (int i = 0; i < parentheses.length; i++) {
+      if (isBalanced(parentheses[i])) {
+        results[i] = "Balanced";
+      } else {
+        results[i] = "Not balanced";
+      }
+    }
+
+    return results;
   }
 }
